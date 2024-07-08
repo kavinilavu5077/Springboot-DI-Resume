31e96d46384dc8f3007f10b337262444f77c4b97
Kavinilavu <143887512+kavinilavu5077@users.noreply.github.com>
2024-07-08 15:17:45 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,441 @@
+package com;
+
+public class Employee {
+	public String ename;
+	public String eage;
+	public String edep;
+	public String ecollege;
+	public String eproject1;
+	public String eproject2;
+	public String eskill1;
+	public String eskill2;
+	public String eskill3;
+	public String eskill4;
+	public String eskill5;
+	public String eskill6;
+	public String eskill7;
+	public String einternship1;
+	public String einternship2;
+	public String einternship3;
+	public String ecertificate;
+	public String ecertificate1;
+	public String ecertificate2;
+	public String econtact;
+	
+	
+	
+	
+
+
+	public String getEname() {
+		return ename;
+	}
+
+
+
+
+
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+
+
+
+
+
+	public String getEage() {
+		return eage;
+	}
+
+
+
+
+
+
+	public void setEage(String eage) {
+		this.eage = eage;
+	}
+
+
+
+
+
+
+	public String getEdep() {
+		return edep;
+	}
+
+
+
+
+
+
+	public void setEdep(String edep) {
+		this.edep = edep;
+	}
+
+
+
+
+
+
+	public String getEcollege() {
+		return ecollege;
+	}
+
+
+
+
+
+
+	public void setEcollege(String ecollege) {
+		this.ecollege = ecollege;
+	}
+
+
+
+
+
+
+	public String getEproject1() {
+		return eproject1;
+	}
+
+
+
+
+
+
+	public void setEproject1(String eproject1) {
+		this.eproject1 = eproject1;
+	}
+
+
+
+
+
+
+	public String getEproject2() {
+		return eproject2;
+	}
+
+
+
+
+
+
+	public void setEproject2(String eproject2) {
+		this.eproject2 = eproject2;
+	}
+
+
+
+
+
+
+	public String getEskill1() {
+		return eskill1;
+	}
+
+
+
+
+
+
+	public void setEskill1(String eskill1) {
+		this.eskill1 = eskill1;
+	}
+
+
+
+
+
+
+	public String getEskill2() {
+		return eskill2;
+	}
+
+
+
+
+
+
+	public void setEskill2(String eskill2) {
+		this.eskill2 = eskill2;
+	}
+
+
+
+
+
+
+	public String getEskill3() {
+		return eskill3;
+	}
+
+
+
+
+
+
+	public void setEskill3(String eskill3) {
+		this.eskill3 = eskill3;
+	}
+
+
+
+
+
+
+	public String getEskill4() {
+		return eskill4;
+	}
+
+
+
+
+
+
+	public void setEskill4(String eskill4) {
+		this.eskill4 = eskill4;
+	}
+
+
+
+
+
+
+	public String getEskill5() {
+		return eskill5;
+	}
+
+
+
+
+
+
+	public void setEskill5(String eskill5) {
+		this.eskill5 = eskill5;
+	}
+
+
+
+
+
+
+	public String getEskill6() {
+		return eskill6;
+	}
+
+
+
+
+
+
+	public void setEskill6(String eskill6) {
+		this.eskill6 = eskill6;
+	}
+
+
+
+
+
+
+	public String getEskill7() {
+		return eskill7;
+	}
+
+
+
+
+
+
+	public void setEskill7(String eskill7) {
+		this.eskill7 = eskill7;
+	}
+
+
+
+
+
+
+	public String getEinternship1() {
+		return einternship1;
+	}
+
+
+
+
+
+
+	public void setEinternship1(String einternship1) {
+		this.einternship1 = einternship1;
+	}
+
+
+
+
+
+
+	public String getEinternship2() {
+		return einternship2;
+	}
+
+
+
+
+
+
+	public void setEinternship2(String einternship2) {
+		this.einternship2 = einternship2;
+	}
+
+
+
+
+
+
+	public String getEinternship3() {
+		return einternship3;
+	}
+
+
+
+
+
+
+	public void setEinternship3(String einternship3) {
+		this.einternship3 = einternship3;
+	}
+
+
+
+
+
+
+	public String getEcertificate() {
+		return ecertificate;
+	}
+
+
+
+
+
+
+	public void setEcertificate(String ecertificate) {
+		this.ecertificate = ecertificate;
+	}
+
+
+
+
+
+
+	public String getEcertificate1() {
+		return ecertificate1;
+	}
+
+
+
+
+
+
+	public void setEcertificate1(String ecertificate1) {
+		this.ecertificate1 = ecertificate1;
+	}
+
+
+
+
+
+
+	public String getEcertificate2() {
+		return ecertificate2;
+	}
+
+
+
+
+
+
+	public void setEcertificate2(String ecertificate2) {
+		this.ecertificate2 = ecertificate2;
+	}
+
+
+
+
+
+
+	public String getEcontact() {
+		return econtact;
+	}
+
+
+
+
+
+
+	public void setEcontact(String econtact) {
+		this.econtact = econtact;
+	}
+
+
+
+
+
+
+	public void displayInfo() {
+		System.out.println("					Resume						");
+		System.out.println("");
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.println("About me:");
+		System.out.println("");
+		System.out.println("Name: " + ename);
+		System.out.println("Age: " + eage);
+		System.out.println("Department: " + edep);
+		System.out.println("College: " + ecollege);
+		System.out.println("");
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.println("Projects:");
+		System.out.println("");
+		System.out.println("- 1st Project: " + eproject1);
+		System.out.println("- 2nd Project: " + eproject2);
+		System.out.println("");
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.println("Skills");
+		System.out.println("");
+		System.out.println("- 1st Skill: "+eskill1);
+		System.out.println("- 2nd Skill: "+eskill2);
+		System.out.println("- 3rd Skill: "+eskill3);
+		System.out.println("- 4th Skill: "+eskill4);
+		System.out.println("- 5th Skill: "+eskill5);
+		System.out.println("- 6th Skill: "+eskill6);
+		System.out.println("- 7th Skill: "+eskill7);
+		System.out.println("");
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.println("Internships:");
+		System.out.println("");
+		System.out.println("- 1st Internship: " + einternship1);
+		System.out.println("- 2nd Internship: " + einternship2);
+		System.out.println("- 3rd Internship: " + einternship3);
+		System.out.println("");
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.println("Certificates:");
+		System.out.println("");
+		System.out.println("- Certificate: " + ecertificate);
+		System.out.println("- 2nd Certificate: " + ecertificate1);
+		System.out.println("- 3rd Certificate: " + ecertificate2);
+		System.out.println("");
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.println("Connect with me!!!: ");
+		System.out.println("");
+		System.out.println("Contact: " + econtact);
+		System.out.println("---------------------------------------------------------------------------------------------------");
+         
+	
+	}
+
+}
